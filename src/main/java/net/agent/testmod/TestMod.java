733ef8f63653c5b30c1f26c8c3d528e0e8d3c34b
Flisd <AgentBruceWane@gmail.com>
2024-06-23 13:01:32 -0500

    Learned how to mod a bit of minecraft
    - Added a sapphire
    - Added a "raw sapphire" (dosent "LOOK" raw)
    - Added a creative menu tab

--- src/main/java/net/agent/testmod/TestMod.java
+++ src/main/java/net/agent/testmod/TestMod.java
@@ -1,9 +1,14 @@
 package net.agent.testmod;
 
 import com.mojang.logging.LogUtils;
+import net.agent.testmod.item.ModCreativeModeTabs;
+import net.agent.testmod.item.ModItems;
 import net.minecraft.client.Minecraft;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -22,13 +27,25 @@ public class TestMod {
     public TestMod() {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
+        ModCreativeModeTabs.register(modEventBus);
+
+        ModItems.register(modEventBus);
+
         modEventBus.addListener(this::commonSetup);
 
         MinecraftForge.EVENT_BUS.register(this);
+        modEventBus.addListener(this::addCreative);
     }
 
     private void commonSetup(final FMLCommonSetupEvent event) {
 
+    }
+
+    private void addCreative(BuildCreativeModeTabContentsEvent event){
+        if(event.getTabKey() == CreativeModeTabs.OP_BLOCKS){
+            event.accept(ModItems.SAPPHIRE);
+            event.accept(ModItems.RAW_SAPPHIRE);
+        }
     }
 
 
