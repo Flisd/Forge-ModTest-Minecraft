9d521a6dc9303c219199be259eda5be9ed664cae
Flisd <AgentBruceWane@gmail.com>
2024-07-21 20:39:29 -0500

    fixed tnt sword

--- src/main/java/net/agent/testmod/item/custom/ExplosiveSwordItem.java
+++ src/main/java/net/agent/testmod/item/custom/ExplosiveSwordItem.java
@@ -1,12 +1,16 @@
 package net.agent.testmod.item.custom;
 
+import net.minecraft.core.BlockPos;
 import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
 import net.minecraft.world.InteractionResultHolder;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.SwordItem;
 import net.minecraft.world.item.Tier;
+import net.minecraft.world.item.context.UseOnContext;
+import net.minecraft.world.level.Explosion;
 import net.minecraft.world.level.Level;
 
 import java.util.*;
@@ -30,11 +34,14 @@ public class ExplosiveSwordItem extends SwordItem {
         return super.hurtEnemy(stack, target, attacker);
     }
 
-    @Override
-    public InteractionResultHolder<ItemStack> use(Level world, Player player, InteractionHand hand) {
+    public InteractionResult useOn(UseOnContext useOnContext) {
+        BlockPos blockPos = useOnContext.getClickedPos();
+        Player player = useOnContext.getPlayer();
+        Level world = useOnContext.getLevel();
+        InteractionHand hand = useOnContext.getHand();
         ItemStack stack = player.getItemInHand(hand);
         // Create a small explosion at the player's location
-        world.explode(null, player.getX(), player.getY(), player.getZ(), 5.0F, Level.ExplosionInteraction.BLOCK);
+        world.explode(null, player.getX(), player.getY(), player.getZ(), 3.0F, Level.ExplosionInteraction.BLOW);
         // Damage the item in the player's hand
         if (stack.isDamageableItem()) {
             stack.hurtAndBreak(1, player, (p) -> p.broadcastBreakEvent(hand));
@@ -42,6 +49,7 @@ public class ExplosiveSwordItem extends SwordItem {
                 stack.shrink(1);
             }
         }
-        return InteractionResultHolder.success(stack);
+        return InteractionResult.SUCCESS;
     }
+
 }
